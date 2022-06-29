ad0b2957bb3fac9eac1473a9e93ec9b405903513
AndresRoman <naroman@utpl.edu.ec>
2022-06-28 21:48:44 -0500

    feat: react-routes fail

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
+import { Button } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Contacto from "./pages/Contacto";
+import QuienSoy from "./pages/QuienSoy";
 import "./App.css";
 
 function App() {
-  const [stateCar, setStateCar] = useState(true);
-  const [contar, setContar] = useState(0);
+  // const [stateCar, setStateCar] = useState(true);
+  // const [contar, setContar] = useState(0);
 
-  useEffect(() => {
-    console.log("Total de clicks: ", contar);
-  }, [contar]);
+  // useEffect(() => {
+  //   console.log("Total de clicks: ", contar);
+  // }, [contar]);
 
-  const encenderApagar = () => {
-    setStateCar((prevValue) => !prevValue);
-    setContar(contar + 1);
-  };
+  // const encenderApagar = () => {
+  //   setStateCar((prevValue) => !prevValue);
+  //   setContar(contar + 1);
+  // };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>El coche está: {stateCar ? "Encendido" : "Apagado"} </h3>
-        <h4>Clicks: {contar}</h4>
-        <button onClick={encenderApagar}>Encender-Apagar</button>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <div>
+          <Link to="/contacto">
+            <Button>Contacto</Button>
+          </Link>
+          <Link to="/quien-soy">
+            <Button>QuienSoy</Button>
+          </Link>
+        </div>
+        <Routes>
+          <Route path="/contacto">
+            <Contacto />
+          </Route>
+          <Route path="/quien-soy">
+            <QuienSoy />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
